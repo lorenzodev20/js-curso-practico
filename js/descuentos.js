bc74f2314b1b4ac398da421147c3c2739fdebca3
Lorenzo Millennium <lorenzorojo@softwaremillenium.com>
2022-02-22 16:21:05 -0500

    Fin ejercicios de descuentos

--- js/descuentos.js
+++ js/descuentos.js
@@ -16,22 +16,14 @@ const coupons = [
   },
 ];
 
-const isCouponValueValid = function (coupon) {
-  return coupon.name === couponValue;
-};
-
-const userCoupon = coupons.find(isCouponValueValid);
-
-if (!userCoupon) {
-  alert("El cupón " + userCouponValue + "no es válido");
-}
 // Funcion Base
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
-
+  
   return precioConDescuento;
 }
+
 
 function onClickButtonPriceDiscount() {
   const inputPrice = document.getElementById("InputPrice");
@@ -40,9 +32,44 @@ function onClickButtonPriceDiscount() {
   const inputDiscount = document.getElementById("InputDiscount");
   const discountValue = inputDiscount.value;
 
-  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
 
   const resultP = document.getElementById("ResultP");
   resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  
+}
+
+function onClickButtonPriceDiscountCoupon(){
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+  
+  const inputDiscount = document.getElementById("InputDiscount");
+  const discountValue = inputDiscount.value;
+  
+  const InputCuponCode = document.getElementById("InputCuponCode");
+  const userCouponValue = InputCuponCode.value;
+  
+  const isCouponValueValid = function (coupon) {
+    return coupon.name === userCouponValue;
+  };
+  
+  const userCoupon = coupons.find(isCouponValueValid);
+  
+  if (!userCoupon) {
+    alert("El cupón " + userCouponValue + "no es válido");
+  } else {
+    const descuento = userCoupon.discount;
+
+    const precioConDescuento = calcularPrecioConDescuento(
+      priceValue,
+      descuento
+    );
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }
 }
 
